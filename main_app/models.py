59c38d6a65aaad25a0fa8082f9e83c3e2160fc6f
Khanh King <khanhnphan15@yahoo.com>
2023-08-14 23:12:39 -0400

    part4 with bonus

--- main_app/models.py
+++ main_app/models.py
@@ -1,16 +1,43 @@
 from django.db import models
-# Import the reverse function
 from django.urls import reverse
+from datetime import date
+
+MEALS = (
+    ("B", "Breakfast"),
+    ("L", "Lunch"),
+    ("D", "Dinner"),
+)
+
 
 class Finch(models.Model):
     name = models.CharField(max_length=100)
-    color = models.CharField(max_length=100)  # Corrected this line
+    color = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
 
     def __str__(self):
-        return f'{self.name} ({self.id})'
-     # Add this method
+        return f"{self.name} ({self.id})"
+
+    # Add this method
     def get_absolute_url(self):
-        return reverse('detail', kwargs={'finch_id': self.id})
+        return reverse("detail", kwargs={"finch_id": self.id})
+
+    def fed_for_today(self):
+        print("Today's feedings:", self.feeding_set.filter(date=date.today()))
+        print("Total meals:", len(MEALS))
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+
+
+class Feeding(models.Model):
+    date = models.DateField("Feeding Date")
+    meal = models.CharField(max_length=1, choices=MEALS, default=MEALS[0][0])
+
+    # Create a finch_id FK
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+
+    class Meta:
+        ordering = ["-date"]
--- main_app/views.py
+++ main_app/views.py
@@ -1,17 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 # Add UpdateView & DeleteView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch  # Import the Finch model
+from .forms import FeedingForm
+from datetime import date
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
 
 def about(request):
     context = {
-        'app_description': "Welcome to the Finch Collector application! This app allows you to view information about different finch species.",
+        "app_description": "Welcome to the Finch Collector application! This app allows you to view information about different finch species.",
     }
-    return render(request, 'about.html', context)
+    return render(request, "about.html", context)
 
 # def all_finches(request):
 #     finches = Finch.objects.all()
@@ -23,33 +25,47 @@ def about(request):
 def finches_index(request):
     finches = Finch.objects.all()
     for i in finches:
-        a = 'a'
-    return render(request, 'finches/index.html', {
-        'finches': finches
-    })
+        a = "a"
+    return render(request, "finches/index.html", {"finches": finches})
 
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
-    return render(request, 'finches/detail.html', {
-        'finch': finch
-    })
+    # instantiate FeedingForm to be rendered in the template
+    feeding_form = FeedingForm()
+    return render(
+        request, "finches/detail.html", {"finch": finch, "feeding_form": feeding_form}
+    )
 
 class FinchCreate(CreateView):
-  model = Finch
-  fields = '__all__' 
-#   or fields = ['name', 'color', 'breed', 'description', 'age']
- # Special string pattern Django will use
-  success_url = '/finches/{finch_id}'
-  # Or if you wanted to redirect to the index page
-  # success_url = '/cats'
+    model = Finch
+    fields = "__all__"
+    #   or fields = ['name', 'color', 'breed', 'description', 'age']
+    # Special string pattern Django will use
+    # success_url = "/finches/{finch_id}"
+    # Or if you wanted to redirect to the index page
+    # success_url = '/cats'
 
 class FinchUpdate(UpdateView):
-  model = Finch
-  # Let's disallow the renaming of a cat by excluding the name field!
-  fields = ['breed', 'color', 'description', 'age']
+    model = Finch
+    # Let's disallow the renaming of a cat by excluding the name field!
+    fields = ["breed", "color", "description", "age"]
 
 class FinchDelete(DeleteView):
-  model = Finch
-  success_url = '/finches'
-
-
+    model = Finch
+    success_url = "/finches"
+    
+def add_feeding(request, finch_id):
+  # create a ModelForm instance using 
+  # the data that was submitted in the form
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # We want a model instance, but
+    # we can't save to the db yet
+    # because we have not assigned the
+    # finch_id FK.
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.date = date.today()  # Set the date to today's date
+    new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
